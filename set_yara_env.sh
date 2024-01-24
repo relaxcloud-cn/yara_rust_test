06ceaa7308d364786585e2866c70bc6524722a2f
yanweijie <yanweijie@relaxcloud.cn>
2024-01-24 16:14:39 +0800

    添加yara示例代码

---
+++ set_yara_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export YARA_LIBRARY_PATH=/usr/local/lib/
+export YARA_INCLUDE_DIR=/usr/local/include/
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use yara::Compiler;
+
+const RULES: &str = r#"
+    rule contains_rust {
+      strings:
+        $rust = "rust" nocase
+      condition:
+        $rust
+    }
+"#;
+
+fn main() {
+    let compiler = Compiler::new().unwrap();
+    let compiler = compiler
+        .add_rules_str(RULES)
+        .expect("Should have parsed rule");
+    let rules = compiler
+        .compile_rules()
+        .expect("Should have compiled rules");
+    let results = rules
+        .scan_mem("I love Rust!".as_bytes(), 5)
+        .expect("Should have scanned");
+    for item in results {
+        println!("{}", item.identifier);
+    }
+}
